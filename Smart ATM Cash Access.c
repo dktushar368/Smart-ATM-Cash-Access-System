ded57253f7bac59d044611599f40690909e48554
Tusar Chandra <130041711+dktushar368@users.noreply.github.com>
2025-05-03 00:34:59 +0600

    Updated by Tusar

--- Smart ATM Cash Access.c
+++ Smart ATM Cash Access.c
@@ -357,6 +357,7 @@ int main()
         if (choice != 5 && choice != 9 && !confirmAction("Do you want to perform another action?"))
         {
             printf("Thank you for using the ATM. Goodbye!\n");
+            printf("Developed by team Syntax Error!\n");
             break;
         }
     }
